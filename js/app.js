c53e3da73b8d5b58bde519d9d9d24025fa632c41
Reem Asaad <70090872+reem-asaad@users.noreply.github.com>
2021-01-07 06:37:41 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,30 @@
+function full() {
+    document.getElementById('column1').style.width = '100%';
+    document.getElementById('column2').style.width = '100%';
+    document.getElementById('column3').style.width = '100%';
+    document.getElementById('column4').style.width = '100%';
+}
+
+function twoColumn() {
+    document.getElementById('column1').style.width = '50%';
+    document.getElementById('column2').style.width = '50%';
+    document.getElementById('column3').style.width = '50%';
+    document.getElementById('column4').style.width = '50%';
+}
+
+function fourColumn() {
+    document.getElementById('column1').style.width = '25%';
+    document.getElementById('column2').style.width = '25%';
+    document.getElementById('column3').style.width = '25%';
+    document.getElementById('column4').style.width = '25%';
+}
+
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+})
